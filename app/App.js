cfc5debbbf2157e1333f6e715dae8730c154c5b8
t-minh <thaiminh.learning@gmail.com>
2021-08-31 10:28:43 +0700

    Check src -> app

--- app/App.js
+++ app/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Amplify</h1>
+        <h1>Hi Amplify</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
